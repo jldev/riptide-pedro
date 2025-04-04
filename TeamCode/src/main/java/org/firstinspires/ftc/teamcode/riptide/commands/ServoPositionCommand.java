e13e4ab29e698d201c798bdb7b52a4604c47e1aa
jldev <josh@thedaringwolf.com>
2025-04-04 18:35:08 -0500

    Added ServoPositionCommand class. This class uses the specs for the Axon servos to compute how long we should wait for the servo to move. The command can be set to wait for the servo to move or to end immediately after setting the servo position.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/riptide/commands/ServoPositionCommand.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.riptide.commands;
+
+import static java.lang.Math.round;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.riptide.subsystems.HorizontalSubsystem;
+
+public class ServoPositionCommand extends CommandBase {
+
+    private final Servo mServo;
+    private Double mDesiredPosition;
+    private Double mStartingPosition;
+
+    private Boolean mWaitForMove = false;
+    private long mMoveEndTime;
+
+    static private double MS_TIME_PER_DEGREE = 140/60; // This is worst case for Axon servos
+    static private double SERVO_DEGREES_OF_ROTATION = 355; // we program all ours to max rotation, if this isn't the case this needs to be changed
+
+    public ServoPositionCommand(Servo servo, Double position, Boolean waitForMove) {
+        this.mServo = servo;
+        this.mDesiredPosition = position;
+        this.mStartingPosition = servo.getPosition();
+        this.mWaitForMove = waitForMove;
+    }
+
+    @Override
+    public void initialize() {
+       this.mServo.setPosition(mDesiredPosition);
+       double moveDelta = Math.abs(mDesiredPosition - mStartingPosition);
+       double deltaDegrees = SERVO_DEGREES_OF_ROTATION * moveDelta;
+       this.mMoveEndTime = System.currentTimeMillis() + round(deltaDegrees * MS_TIME_PER_DEGREE);
+    }
+
+    @Override
+    public void end(boolean interrupted) {}
+
+    @Override
+    public boolean isFinished() {
+        return Thread.currentThread().isInterrupted() || !mWaitForMove || System.currentTimeMillis() > this.mMoveEndTime;
+    }
+}
